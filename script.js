0d8b5eddeeaa163218fd073f0466042fb728f7fe
alexholad <alexej.holad@gmail.com>
2022-06-22 10:43:24 +0200

    added functionality to next and previous buttons

--- script.js
+++ script.js
@@ -1,27 +1,91 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
-const prevBtn = document.getElementById("prev")
-const playBtn = document.getElementById("play")
-const nextBtn = document.getElementById("next")
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
 
+// Music
+const songs = [
+  {
+    name: "admiralbob-1",
+    displayName: "Covid for Christmas (ft. airtone)",
+    artist: "Admiral Bob",
+  },
+  {
+    name: "admiralbob-2",
+    displayName: "Victoria",
+    artist: "Admiral Bob",
+  },
+  {
+    name: "admiralbob-3",
+    displayName: "Madness",
+    artist: "Admiral Bob",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
 
 // Check if Playing
 let isPlaying = false;
 
 // Play
 function playSong() {
-    isPlaying = true;
-    playBtn.classList.replace('fa-play', 'fa-pause');
-    playBtn.setAttribute('title', 'Pause');
-    music.play()
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
 }
 
 // Play
 function pauseSong() {
-    isPlaying = false;
-    playBtn.classList.replace('fa-pause', 'fa-play');
-    playBtn.setAttribute('title', 'Play');
-    music.pause()
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
 }
 
 // Play or Pause Event Listener
-playBtn.addEventListener("click", () => isPlaying ? pauseSong() : playSong());
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+//  On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
